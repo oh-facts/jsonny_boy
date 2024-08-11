717f4b284021264d2d14aebdbf0819b8953f3078
oh-facts <as17793@nyu.edu>
2024-08-11 05:34:18 +0530

    bat script problemo

--- benchmarks/jsonny_boy/build.bat
+++ benchmarks/jsonny_boy/build.bat
@@ -3,5 +3,9 @@ setlocal
 cd /D "%~dp0"
 
 if not exist "out" mkdir "out"
+
+call "..\..\build.bat" release
+copy "..\..\out\jsonny_boy.lib" "out\jsonny_boy.lib"
+
 clang -O3 -Wno-writable-strings -lout/jsonny_boy main.cpp -o out/test.exe
 .\out\test.exe
